e944da95511645223ed117e443b31aac03a62890
Justin Li <jli.justinli@gmail.com>
2012-11-25 13:21:45 -0500

    Initial pathfinding interface

--- final/control.py
+++ final/control.py
@@ -4,11 +4,11 @@ from sensors import *
 class Control:
 
 	def __init__(self, _robot):
-		self.robot = _robot
-		self.robot.grid = [[0 for i in xrange(10)] for j in xrange(10)]
+		_robot.pathfinder = pathfinder.Pathfinder()
 		self.movement = movement.Movement(_robot)
-		s = Sensors(r)
-		m = Movement(r, s)
+		s = Sensors(_robot)
+		m = Movement(_robot, s)
+		self.robot = _robot
 
 	def run(self):
 		self.movement.goto(10, 10)
